da2c78fdf920df8d80d0186c7ca2edb90ab93899
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-05-16 02:57:44 +0530

    APP Build

---
+++ android/app/src/main/java/com/myapp/mobile/MainActivity.java
@@ -0,0 +1,5 @@
+package com.myapp.mobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myapp.mobile',
+  appName: 'my-app',
+  webDir: 'out'
+};
+
+export default config;
